3c605949cbe7e18da2c80bf94c04b6dc289d586d
Md-Zehad-Sarkar <zehadsarkar22@outlook.com>
2023-11-25 16:50:35 +0600

    code remove

--- src/app/modules/user/user.validation.ts
+++
@@ -1,8 +0,0 @@
-export type TUser = {
-  id: string;
-  password: string;
-  needPasswordChanged: boolean;
-  role: 'admin' | 'student' | 'faculty';
-  status: 'in-progress' | 'blocked';
-  isDeleted: boolean;
-};
