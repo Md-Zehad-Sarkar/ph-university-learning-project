43e686858bab657bb88dd20201ee9be78e02fd23
Md-Zehad-Sarkar <zehadsarkar22@outlook.com>
2023-12-01 10:52:57 +0600

    success code before transaction and rollback

--- src/app/utils/sendResponse.ts
+++ src/app/utils/sendResponse.ts
@@ -5,6 +5,8 @@ type TResponse<T> = {
   message: string;
   data: T;
 };
+
+//send response
 export const sendResponse = <T>(res: Response, data: TResponse<T>) => {
   res.status(data?.statusCode).json({
     success: data?.success,
